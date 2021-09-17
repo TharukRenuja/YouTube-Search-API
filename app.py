4b796a4d4efad1fe5f8bec61ddd5bfd2fe12a378
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-17 11:50:26 +0530

    feature: added home (#1)

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ from youtubesearchpython import *
 app = Flask(__name__)
 
 
+@app.route("/")
+def home():
+    return "Documentation:- <a href='https://github.com/FayasNoushad/YouTube-Search-API'>YouTube-Search-API</a>"
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
