b0071b60865d1a99407730dde1af821be1dc2482
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-17 11:18:10 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,5 @@
+# Made with python3
+# (C) @FayasNoushad
+# Copyright permission under MIT License
+# All rights reserved by FayasNoushad
+# License -> https://github.com/FayasNoushad/YouTube-Search-API/blob/main/LICENSE
