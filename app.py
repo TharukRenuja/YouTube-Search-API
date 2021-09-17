a08ea08397991b4d0d3de9a654d83ae569443c9c
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-17 11:57:22 +0530

    feature: added playlist search (#4)

--- app.py
+++ app.py
@@ -36,6 +36,16 @@ def videos():
     return jsonify(videos.result())
 
 
+@app.route("/playlists/", methods=['GET'])
+def playlists():
+    query = request.args.get('query')
+    limit = int(request.args.get('limit')) if request.args.get('limit') else 100
+    language = request.args.get('language') if request.args.get('language') else None
+    region = request.args.get('region') if request.args.get('region') else None
+    playlists = PlaylistsSearch(query, limit=limit, language=language, region=region)
+    return jsonify(playlists.result())
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
