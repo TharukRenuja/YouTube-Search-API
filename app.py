17d63092382209f3eec091fb0eb9965d3fc09212
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-11-27 14:50:23 +0530

    Remove copyright information

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# Made with python3
-# (C) @FayasNoushad
-# Copyright permission under MIT License
-# All rights reserved by FayasNoushad
-# License -> https://github.com/FayasNoushad/YouTube-Search-API/blob/main/LICENSE
-
 from flask import Flask, redirect, render_template, request, jsonify, json
 from youtubesearchpython import *
 
